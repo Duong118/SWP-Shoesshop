db082c41a5dbf08c9d823e0374934f59627132d6
Duong118 <nguyenchiduongp1@gmail.com>
2025-06-23 21:33:55 +0700

    edit

---
+++ New folder/src/main/java/com/swp/shoesshop/config/MailConfig.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.swp.shoesshop.config;
+
+public class MailConfig {
+    public static final String senderMail = "duongncce180374@fpt.edu.vn";
+    public static final String senderPassword = "gqkfcksqsuoeytwg";
+}
