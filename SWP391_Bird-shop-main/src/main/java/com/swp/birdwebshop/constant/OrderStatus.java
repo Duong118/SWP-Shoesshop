965245aa5c7fc523c9d61bbdb83575335e543422
LamVHCE171024 <lamvhce171024@fpt.edu.vn>
2025-06-24 20:44:03 +0700

    OrderStatus

---
+++ SWP391_Bird-shop-main/src/main/java/com/swp/birdwebshop/constant/OrderStatus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.swp.birdwebshop.constant;
+
+/**
+ *
+ * @author huynh
+ */
+public enum OrderStatus {
+    SUCCESS,
+    APPROVED,
+    FAIL,
+    PROCESSING;
+}
