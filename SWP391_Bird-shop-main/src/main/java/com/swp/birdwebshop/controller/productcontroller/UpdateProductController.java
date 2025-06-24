070e860e5abf47c1c385d57fc6735057e6400a21
Văn Thương <thuong200422@gmail.com>
2025-06-24 16:46:16 +0700

    Edit Shoes Delete Shoes View Shoes (Admin) Manage Shoe Sizes Add Shoe Size Edit Shoe Size Delete Shoe Size View Shoe Size

---
+++ SWP391_Bird-shop-main/src/main/java/com/swp/birdwebshop/controller/productcontroller/UpdateProductController.java
@@ -0,0 +1,227 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.swp.birdwebshop.controller.productcontroller;
+
+import com.swp.birdwebshop.constant.ProductStatus;
+import com.swp.birdwebshop.constant.Regex;
+import com.swp.birdwebshop.dao.impldao.CategoryDAOImpl;
+import com.swp.birdwebshop.dao.impldao.ProductDAOImpl;
+import com.swp.birdwebshop.model.Category;
+import com.swp.birdwebshop.model.Product;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.regex.Pattern;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Milo
+ */
+@WebServlet(name = "UpdateProductController", urlPatterns = {"/UpdateProductController"})
+public class UpdateProductController extends HttpServlet {
+
+    private static final String ERROR = "edit_product.jsp";
+    private static final String SUCCESS = "ViewDetailProductController";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        ProductDAOImpl proDao = new ProductDAOImpl();
+        CategoryDAOImpl cateDao = new CategoryDAOImpl();
+        HttpSession session = request.getSession();
+        try {
+            List<Category> cates = cateDao.viewCate();
+            session.setAttribute("VIEW_CATE", cates);
+            int id = (Integer) session.getAttribute("ID");
+            int originalPrice = Integer.parseInt(request.getParameter("originalPrice"));
+            int quantity = Integer.parseInt(request.getParameter("quantity"));
+            int point = Integer.parseInt(request.getParameter("point"));
+            String name = request.getParameter("name").trim();
+            String img = request.getParameter("img").trim();
+            String discount = request.getParameter("dis");
+            int discountPrice;
+            String description = request.getParameter("description");
+            String shortdescript = request.getParameter("shortdescript");
+            //Xử lý cateId
+            String catetype = request.getParameter("catetype");
+            String catename = request.getParameter("catename");
+            int cateid = 0;
+            if (!catename.equals("Khác")) {
+                List<Category> catetypes = cateDao.getByCateType(catetype);
+                for (Category cate : catetypes) {
+                    String cname = cate.getName();
+                    if (cname.equals(catename)) {
+                        cateid = cate.getId();
+                        break;
+                    }
+                }
+            } else if (catename.equals("Khác")) {
+                catename = request.getParameter("newcatename").trim();
+                boolean checkCateDuplicate = cateDao.checkCateDuplicate(catename);
+                request.setAttribute("isDuplicated", checkCateDuplicate);
+                if (!checkCateDuplicate) {
+                    Category cate = new Category(catename, catetype);
+                    boolean checknewcate = cateDao.createCate(cate);
+                    if (checknewcate) {
+                        cateid = cateDao.getNewCateId();
+                        cates = cateDao.viewCate();
+                        session.setAttribute("VIEW_CATE", cates);
+                        request.setAttribute("newCate", true);
+                    } else if (!checknewcate) {
+                        request.setAttribute("PRODUCT_ERROR", "Có lỗi với phân loại sản phẩm");
+
+                    }
+                } else {
+                    request.setAttribute("PRODUCT_ERROR", "Loại sản phẩm này đã tồn tại");
+                }
+            }
+            //Xử lý status
+            ProductStatus status = null;
+            if (quantity > 0) {
+                status = ProductStatus.INSTOCK;
+            } else if (quantity == 0) {
+                status = ProductStatus.OUTOFSTOCK;
+            }
+
+            //validate
+            boolean checkValidation = true;
+            if (discount != null) {
+                discountPrice = Integer.parseInt(request.getParameter("discountPrice"));
+                if (discountPrice > originalPrice || discountPrice < 0) {
+                    request.setAttribute("PRODUCT_ERROR", "Giá giảm không hợp lệ rùi");
+                    request.setAttribute("checked", true);
+                    checkValidation = false;
+                }
+            } else {
+                discountPrice = originalPrice;
+            }
+            if (name.length() > 200 || name.length() < 2) {
+                request.setAttribute("PRODUCT_ERROR", "Tên sản phẩm không hợp lệ");
+                checkValidation = false;
+            }
+            if (originalPrice < 999) {
+                request.setAttribute("PRODUCT_ERROR", "Giá không hợp lệ rùi");
+                checkValidation = false;
+            }
+            if (quantity < 0) {
+                request.setAttribute("PRODUCT_ERROR", "Số lượng phải lớn hơn 0 nha");
+                checkValidation = false;
+            }
+            if (point < 0) {
+                request.setAttribute("PRODUCT_ERROR", "Điểm phải lớn hơn 0 nha");
+                checkValidation = false;
+            }
+            Pattern urlPattern = Pattern.compile(Regex.IMAGE_URL_PATTERN);
+            if (!urlPattern.matcher(img).find()) {
+                request.setAttribute("PRODUCT_ERROR", "Vui lòng nhập link hình ảnh hợp lệ!");
+                checkValidation = false;
+            } else {
+                try {
+                    Image image = ImageIO.read(new URL(img));
+                    if (image == null) {
+                        request.setAttribute("PRODUCT_ERROR", "Không tìm thấy link hình ảnh này");
+                        checkValidation = false;
+                    }
+                } catch (IOException e) {
+                    request.setAttribute("PRODUCT_ERROR", "Lỗi khi đọc hình ảnh từ URL");
+                    checkValidation = false;
+                }
+            }
+
+                if (checkValidation) {
+                    Product product = new Product(id, name, img, originalPrice, discountPrice, description, shortdescript, point, cateid, 0, status);
+                    boolean checkUpdate = proDao.checkUpdate(product);
+                    if (checkUpdate) {
+                        request.setAttribute("UPDATE_SUCCESS", "Chỉnh sửa sản phẩm thành công!!!");
+                        url = SUCCESS;
+                        request.setAttribute("PRODUCT", product);
+                        Category cateup = new Category(cateid, catename, catetype);
+                        request.setAttribute("CATE", cateup);
+                        request.setAttribute("PRODUCT_ERROR", null);
+                    } else {
+                        request.setAttribute("UPDATE_SUCCESS", null);
+                        request.setAttribute("PRODUCT", product);
+                        Category cateup = new Category(cateid, catename, catetype);
+                        request.setAttribute("CATE", cateup);
+                        request.setAttribute("PRODUCT_ERROR", "Ủa là update dữ chưa z");
+                    }
+                } else {
+                    request.setAttribute("UPDATE_SUCCESS", null);
+                    Product product = new Product(id, name, img, originalPrice, discountPrice, description, shortdescript, point, cateid, 0, status);
+                    request.setAttribute("PRODUCT", product);
+                    Category cateup = new Category(cateid, catename, catetype);
+                    request.setAttribute("CATE", cateup);
+                }
+            }catch (Exception e) {
+            log("Error at CreateProductController" + e.toString());
+            request.setAttribute("PRODUCT_ERROR", "Có lỗi xảy ra rồi .-.");
+
+        }finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+        }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        /**
+         * Handles the HTTP <code>GET</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doGet
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo
+        
+            () {
+        return "Short description";
+        }// </editor-fold>
+
+    }
